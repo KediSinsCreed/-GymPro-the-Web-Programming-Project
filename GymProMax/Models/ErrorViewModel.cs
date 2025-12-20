f249c2cca63a50444671176a5284c6ab2464262d
Emir Abdullah Önal <160718188+KediSinsCreed@users.noreply.github.com>
2025-12-21 02:05:28 +0300

    Delete GymProMax directory

--- GymProMax/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GymProMax.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- GymProMax/README.md
+++
@@ -1,2 +0,0 @@
-G231210021 Emir Abdullah Önal
-dsgfsgfsfhfggfhdf falanfilaniter milan barca real madrid fenerbahçe galatasaray beşiktaş trabzonspor sivasspor ankaragücü kayserispor
